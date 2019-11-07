9166e2631b70b0e4cbe3bf01fbcb89e2304be4a7
J.Costa <costa.16@yahoo.com>
2019-11-07 17:14:39 -0500

    [updated main.py for PyBank, exporting a .txt and a .cvs]

--- PyBank/main.py
+++ PyBank/main.py
@@ -58,7 +58,8 @@ print(f"-------------------------------------\n"
       f"Greatest Increase in Profits: {month_increase} ($ {greatest_increase})\n"
       f"Greatest Decrease in Profits: {month_decrease} ($ {greatest_decrease})")
 
-# Export a text file with the results
+# Export a text (.txt) file with the results
+
 output_path = os.path.join("budget_analysis.txt")
 with open(output_path, 'w') as txtfile:
     txtfile.write(f"-------------------------------------\n"
@@ -69,3 +70,17 @@ with open(output_path, 'w') as txtfile:
                   f"Average Change: $ {average_loss_profit}\n"
                   f"Greatest Increase in Profits: {month_increase} ($ {greatest_increase})\n"
                   f"Greatest Decrease in Profits: {month_decrease} ($ {greatest_decrease})")
+
+# Export a text (.csv) file with the results
+
+output_path = os.path.join("budget_analysis.csv")
+with open(output_path, 'w', newline='') as csvfile:
+     budget_analysis = csv.writer(csvfile, delimiter=',')
+
+     budget_analysis.writerow(
+         ['Total_Months', 'Total_Net', 'Average_Change', 'Month_Great_Increase',
+          'Value_Great_Increase', 'Month_Great_Decrease',
+          'Value_Great_Decrease'])
+     budget_analysis.writerow(
+         [total_months, net_loss_profit, average_loss_profit, month_increase, greatest_increase, month_decrease,
+          greatest_decrease])
